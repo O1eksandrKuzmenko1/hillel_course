f60bf17d5d60fae4a87c867b7bc7c6f7c57461be
O1eksandrKuzmenko1 <a.kuzmenko.07.98@gmail.com>
2024-07-31 14:41:43 +0200

    homework#2

---
+++ Homework_2/homework_2_1.py
@@ -0,0 +1,17 @@
+
+
+a = int(input("enter your number: "))
+b = a // 1000
+c = a // 100
+d = a // 10
+
+divider = 10
+
+
+
+print(b)
+print(c % divider)
+print(d % divider)
+print(a % divider)
+
+
---
+++ Homework_2/homework_2_2.py
@@ -0,0 +1,19 @@
+number = int(input("enter your number: "))
+a = 0
+divader = 10
+
+a = a * divader + number % divader
+number //= divader
+
+a = a * divader + number % divader
+number //= divader
+
+a = a * divader + number % divader
+number //= divader
+
+a = a * divader + number % divader
+number //= divader
+
+a = a * divader + number % divader
+number //= divader
+print(a)
